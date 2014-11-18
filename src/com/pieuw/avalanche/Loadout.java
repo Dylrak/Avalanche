95e995abdb57d11801916665c167710e407f8808
dylrak <dylrak@hotmail.com>
2014-11-18 21:13:54 +0100

    moar loadout

--- src/com/pieuw/avalanche/Loadout.java
+++ src/com/pieuw/avalanche/Loadout.java
@@ -19,13 +19,21 @@ import android.widget.Button;
 
 public class Loadout extends Activity {
 	String TAG = Loadout.class.getName(), FILENAME = "loadout.txt", inValues, concatOutValues;
-	String[] outValues = new String [] {"0", "0"};
+	int[] outValues = new int [] {0, 0};
 		/**1. Lava Immunity**/ 
 		/**2. Block Immunity**/ 
 		/**3. Double Jump**/
 		/**4. Icarus Boots**/
-	int i;
+	int i, current_passive;
 	File file = new File (FILENAME);
+	
+	final Button passive_1 = (Button) findViewById(R.id.passive_1);
+	final Button passive_2 = (Button) findViewById(R.id.passive_2);
+	final Button lavaImmunity = (Button) findViewById(R.id.lava_immunity);
+	final Button blockImmunity = (Button) findViewById(R.id.block_immunity);
+	final Button doubleJump = (Button) findViewById(R.id.double_jump);
+	final Button icarusBoots = (Button) findViewById(R.id.icarus_boots);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -42,71 +50,155 @@ public class Loadout extends Activity {
 		/**Check whether or not file contains anything; if not, write default values to file.**/
 		if (file.exists() && file.canRead()) {
 			if (inValues.matches("^\\d")) {
-				outValues = inValues.split("\n");
+				String[] tempArray = inValues.split("\n");
+				i = 0;
+				for (String str: tempArray) {
+					outValues[i++] = Integer.parseInt (str);
+				}
+				
 			}
 		} else {
-			for (i=0; i <= outValues.length; i++) {
-				concatOutValues.concat(outValues[i] + "\n");
-			}
-			writeToFile(concatOutValues);
+			printToFile();
 		}
 		
-        final Button lavaImmunity = (Button) findViewById(R.id.lava_immunity);
+		refreshPassives ();
+		
         lavaImmunity.setOnClickListener(new View.OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		
+        		if (outValues[0] != 1) {
+        			passive_1.setEnabled (true);
+        		}
+        		if (outValues[1] != 1) {
+        			passive_2.setEnabled (true);
+        		}
         	}
         
         });
         
-        final Button startGame = (Button) findViewById(R.id.start_game);
-        startGame.setOnClickListener(new View.OnClickListener() {
+        blockImmunity.setOnClickListener(new View.OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		
+        		if (outValues[0] != 2) {
+        			passive_1.setEnabled (true);
+        		}
+        		if (outValues[1] != 2) {
+        			passive_2.setEnabled (true);
+        		}
+        	}
+        
+        });
+        
+        doubleJump.setOnClickListener(new View.OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		if (outValues[0] != 3) {
+        			passive_1.setEnabled (true);
+        		}
+        		if (outValues[1] != 3) {
+        			passive_2.setEnabled (true);
+        		}
+        	}
+        
+        });
+        
+        icarusBoots.setOnClickListener(new View.OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		if (outValues[0] != 4) {
+        			passive_1.setEnabled (true);
+        		}
+        		if (outValues[1] != 4) {
+        			passive_2.setEnabled (true);
+        		}
+        	}
+        
+        });
+        passive_1.setOnClickListener(new View.OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		outValues[0] = current_passive;
+        		printToFile();
+        		refreshPassives ();
+        	}
+        
+        });
+        
+        passive_2.setOnClickListener(new View.OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		outValues[1] = current_passive;
+        		printToFile();
+        		refreshPassives ();
         	}
         
         });
 	}
 	
-	private void writeToFile(String data) {
+	public void refreshPassives () {
+		passive_1.setEnabled(false);
+		passive_2.setEnabled(false);
+		if (outValues[0] == 1) {
+			passive_1.setBackgroundResource(R.drawable.lava_immunity);
+		} else if (outValues[0] == 2) {
+			passive_1.setBackgroundResource(R.drawable.block_immunity);
+		} else if (outValues[0] == 3) {
+			passive_1.setBackgroundResource(R.drawable.double_jump);
+		} else if (outValues[0] == 4) {
+			passive_1.setBackgroundResource(R.drawable.icarus_boots);
+		}
+		if (outValues[1] == 1) {
+			passive_2.setBackgroundResource(R.drawable.lava_immunity);
+		} else if (outValues[1] == 2) {
+			passive_2.setBackgroundResource(R.drawable.block_immunity);
+		} else if (outValues[1] == 3) {
+			passive_2.setBackgroundResource(R.drawable.double_jump);
+		} else if (outValues[1] == 4) {
+			passive_2.setBackgroundResource(R.drawable.icarus_boots);
+		}
+	}
+	
+	private void printToFile() {
+		for (i=0; i <= outValues.length; i++) {
+			concatOutValues.concat(outValues[i] + "\n");
+		}
         try {
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(FILENAME, Context.MODE_PRIVATE));
-            outputStreamWriter.write(data);
+            outputStreamWriter.write(concatOutValues);
             outputStreamWriter.close();
         }
         catch (IOException e) {
             Log.e(TAG, "File write failed: " + e.toString());
         } 
     }
-        private String readFromFile() {
-            
-            String ret = "";
+	
+    private String readFromFile() {
+        
+        String ret = "";
+         
+        try {
+            InputStream inputStream = openFileInput(FILENAME);
              
-            try {
-                InputStream inputStream = openFileInput(FILENAME);
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
                  
-                if ( inputStream != null ) {
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                    String receiveString = "";
-                    StringBuilder stringBuilder = new StringBuilder();
-                     
-                    while ( (receiveString = bufferedReader.readLine()) != null ) {
-                        stringBuilder.append(receiveString);
-                    }
-                     
-                    inputStream.close();
-                    ret = stringBuilder.toString();
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
                 }
+                 
+                inputStream.close();
+                ret = stringBuilder.toString();
             }
-            catch (FileNotFoundException e) {
-                Log.e(TAG, "File not found: " + e.toString());
-            } catch (IOException e) {
-                Log.e(TAG, "Can not read file: " + e.toString());
-            }
-     
-            return ret;
         }
+        catch (FileNotFoundException e) {
+            Log.e(TAG, "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e(TAG, "Can not read file: " + e.toString());
+        }
+ 
+        return ret;
+    }
 }
