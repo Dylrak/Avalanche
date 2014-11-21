ea97506d548ed05bfe47967e72f41ef27b9b0176
dylrak <dylrak@hotmail.com>
2014-11-21 18:54:59 +0100

    Added SharedPrefs

--- src/com/pieuw/avalanche/Loadout.java
+++ src/com/pieuw/avalanche/Loadout.java
@@ -1,31 +1,17 @@
 package com.pieuw.avalanche;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 import android.app.Activity;
-import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
 public class Loadout extends Activity {
-	String TAG = Loadout.class.getName(), FILENAME = "loadout.txt", inValues, concatOutValues;
-	int[] outValues = new int [] {0, 0};
-		/**1. Lava Immunity**/ 
-		/**2. Block Immunity**/ 
-		/**3. Double Jump**/
-		/**4. Icarus Boots**/
-	int i, current_passive;
-	File file = new File (FILENAME);
+	public static String FILENAME = "preferences";
+	int passive1, passive2, inValues, i, current_passive;
+	SharedPreferences preferences;
 	
 	final Button passive_1 = (Button) findViewById(R.id.passive_1);
 	final Button passive_2 = (Button) findViewById(R.id.passive_2);
@@ -44,34 +30,24 @@ public class Loadout extends Activity {
 	    
     	
 		setContentView(R.layout.activity_loadout);
-		
-		inValues = readFromFile();
-		
-		/**Check whether or not file contains anything; if not, write default values to file.**/
-		if (file.exists() && file.canRead()) {
-			if (inValues.matches("^\\d")) {
-				String[] tempArray = inValues.split("\n");
-				i = 0;
-				for (String str: tempArray) {
-					outValues[i++] = Integer.parseInt (str);
-				}
-				
-			}
-		} else {
-			printToFile();
-		}
-		
+
 		refreshPassives ();
+		
+		/**1. Lava Immunity**/ 
+		/**2. Block Immunity**/ 
+		/**3. Double Jump**/
+		/**4. Icarus Boots**/
 		
         lavaImmunity.setOnClickListener(new View.OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		if (outValues[0] != 1) {
+        		if (passive1 != 1) {
         			passive_1.setEnabled (true);
         		}
-        		if (outValues[1] != 1) {
+        		if (passive2 != 1) {
         			passive_2.setEnabled (true);
         		}
+        		current_passive = 1;
         	}
         
         });
@@ -79,12 +55,13 @@ public class Loadout extends Activity {
         blockImmunity.setOnClickListener(new View.OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		if (outValues[0] != 2) {
+        		if (passive1 != 2) {
         			passive_1.setEnabled (true);
         		}
-        		if (outValues[1] != 2) {
+        		if (passive2 != 2) {
         			passive_2.setEnabled (true);
         		}
+        		current_passive = 2;
         	}
         
         });
@@ -92,12 +69,13 @@ public class Loadout extends Activity {
         doubleJump.setOnClickListener(new View.OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		if (outValues[0] != 3) {
+        		if (passive1 != 3) {
         			passive_1.setEnabled (true);
         		}
-        		if (outValues[1] != 3) {
+        		if (passive2 != 3) {
         			passive_2.setEnabled (true);
         		}
+        		current_passive = 3;
         	}
         
         });
@@ -105,20 +83,20 @@ public class Loadout extends Activity {
         icarusBoots.setOnClickListener(new View.OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		if (outValues[0] != 4) {
+        		if (passive1 != 4) {
         			passive_1.setEnabled (true);
         		}
-        		if (outValues[1] != 4) {
+        		if (passive2 != 4) {
         			passive_2.setEnabled (true);
         		}
+        		current_passive = 4;
         	}
         
         });
         passive_1.setOnClickListener(new View.OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		outValues[0] = current_passive;
-        		printToFile();
+        		passive1 = current_passive;
         		refreshPassives ();
         	}
         
@@ -127,8 +105,7 @@ public class Loadout extends Activity {
         passive_2.setOnClickListener(new View.OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		outValues[1] = current_passive;
-        		printToFile();
+        		passive2 = current_passive;
         		refreshPassives ();
         	}
         
@@ -136,69 +113,30 @@ public class Loadout extends Activity {
 	}
 	
 	public void refreshPassives () {
+		preferences = getSharedPreferences(FILENAME, 0);
+		SharedPreferences.Editor editor = preferences.edit();
+		passive1 = preferences.getInt("passive_1", 0);
+		passive2 = preferences.getInt("passive_2", 0);
+		editor.commit();
 		passive_1.setEnabled(false);
 		passive_2.setEnabled(false);
-		if (outValues[0] == 1) {
+		if (passive1 == 1) {
 			passive_1.setBackgroundResource(R.drawable.lava_immunity);
-		} else if (outValues[0] == 2) {
+		} else if (passive1 == 2) {
 			passive_1.setBackgroundResource(R.drawable.block_immunity);
-		} else if (outValues[0] == 3) {
+		} else if (passive1 == 3) {
 			passive_1.setBackgroundResource(R.drawable.double_jump);
-		} else if (outValues[0] == 4) {
+		} else if (passive1 == 4) {
 			passive_1.setBackgroundResource(R.drawable.icarus_boots);
 		}
-		if (outValues[1] == 1) {
+		if (passive2 == 1) {
 			passive_2.setBackgroundResource(R.drawable.lava_immunity);
-		} else if (outValues[1] == 2) {
+		} else if (passive2 == 2) {
 			passive_2.setBackgroundResource(R.drawable.block_immunity);
-		} else if (outValues[1] == 3) {
+		} else if (passive2 == 3) {
 			passive_2.setBackgroundResource(R.drawable.double_jump);
-		} else if (outValues[1] == 4) {
+		} else if (passive2 == 4) {
 			passive_2.setBackgroundResource(R.drawable.icarus_boots);
 		}
 	}
-	
-	private void printToFile() {
-		for (i=0; i <= outValues.length; i++) {
-			concatOutValues.concat(outValues[i] + "\n");
-		}
-        try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(FILENAME, Context.MODE_PRIVATE));
-            outputStreamWriter.write(concatOutValues);
-            outputStreamWriter.close();
-        }
-        catch (IOException e) {
-            Log.e(TAG, "File write failed: " + e.toString());
-        } 
-    }
-	
-    private String readFromFile() {
-        
-        String ret = "";
-         
-        try {
-            InputStream inputStream = openFileInput(FILENAME);
-             
-            if ( inputStream != null ) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String receiveString = "";
-                StringBuilder stringBuilder = new StringBuilder();
-                 
-                while ( (receiveString = bufferedReader.readLine()) != null ) {
-                    stringBuilder.append(receiveString);
-                }
-                 
-                inputStream.close();
-                ret = stringBuilder.toString();
-            }
-        }
-        catch (FileNotFoundException e) {
-            Log.e(TAG, "File not found: " + e.toString());
-        } catch (IOException e) {
-            Log.e(TAG, "Can not read file: " + e.toString());
-        }
- 
-        return ret;
-    }
 }
